f65293e148ed70f2548e99541054fe445125bfb7
kajitomg <elementrixmg@bk.ru>
2022-04-02 00:33:34 +0300

    fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(express.static(path.resolve(__dirname, 'build')))
@@ -12,4 +12,4 @@ app.get('*',
 	}
 )
 
-app.listen(PORT)
+app.listen(PORT, 'localhost')
